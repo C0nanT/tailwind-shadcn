0c8a6906a99426fb6ef1f82e376e0f29f3df96b8
Conan Torres <conantorreswf@gmail.com>
2024-09-10 21:02:41 -0300

    feat(login): começa o form de login

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,98 @@
-import Image from "next/image";
+"use client"; // Isso indica que o componente será renderizado no cliente
 
-export default function Home() {
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { useEffect, useState } from 'react';
+
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [clickLogin, setClickLogin] = useState(false);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log({ email, password });
+
+    setClickLogin(!clickLogin);
+  };
+
+  useEffect(() => {
+    console.log(clickLogin); 
+  }, [clickLogin]);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <div className="flex items-center justify-center min-h-screen bg-gray-950">
+      <div className="w-full max-w-md p-8 space-y-8 rounded-lg shadow-lg bg-green-50">
+        <h2 className="font-mono text-3xl font-bold tracking-wide text-center transition-all duration-300 hover:tracking-widest text-green-950">
+          Login
+        </h2>
+        <form noValidate onSubmit={handleLogin} className="space-y-6">
+          <div>
+            <Label
+              className="font-mono text-lg tracking-wider duration-300 text-green-950 hover:tracking-widest transition-alld"
+              htmlFor="email"
+            >
+              Email
+            </Label>
+            <Input
+              id="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              placeholder="Digite seu email"
+              className="flex mt-1 transition-all duration-500 bg-white border-2 border-green-500 focus:ring-2 focus:valid:ring-green-500 valid:text-green-600 invalid:border-gray-500 invalid:text-gray-600 focus:invalid:border-gray-500 focus:invalid:ring-gray-500 hover:pl-4"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+          </div>
+          <div>
+            <Label
+              className="font-mono text-lg tracking-wider duration-300 text-green-950 hover:tracking-widest transition-alld"
+              htmlFor="password"
+            >
+              Password
+            </Label>
+            <Input
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              placeholder="Digite sua senha"
+              className="flex mt-1 transition-all duration-500 bg-white border-2 border-green-500 focus:ring-2 focus:valid:ring-green-500 valid:text-green-600 invalid:border-gray-500 invalid:text-gray-600 focus:invalid:border-gray-500 focus:invalid:ring-gray-500 hover:pl-4"
+            />
+          </div>
+          {clickLogin ? (
+            <Button
+              type="submit"
+              className="w-full font-mono text-lg tracking-wider transition-all duration-300 bg-green-900 hover:bg-green-800 text-green-50 hover:tracking-widest"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m4.5 12.75 6 6 9-13.5"
+                />
+              </svg>
+            </Button>
+          ) : (
+            <Button
+              type="submit"
+              className="w-full font-mono text-lg tracking-wider transition-all duration-300 bg-green-900 hover:bg-green-800 text-green-50 hover:tracking-widest"
+            >
+              Entrar
+            </Button>
+          )}
+        </form>
+      </div>
     </div>
   );
 }
